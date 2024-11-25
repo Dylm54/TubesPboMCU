c26852a8c0b57b5dcd8d21c8081e72bc00a4607a
Muhammad Fakhrul Hizrian <136154375+Rianyann06@users.noreply.github.com>
2024-11-25 14:03:27 +0700

    Paket Mcu Beres

--- web/src/main/java/com/mcu/web/models/PaketMCU.java
+++ web/src/main/java/com/mcu/web/models/PaketMCU.java
@@ -1,5 +1,17 @@
 package com.mcu.web.models;
 
+import lombok.*;
+import org.springframework.data.annotation.Id;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PaketMCU {
+    @Id
+    private String idPaket;
+    private String namaPaket;
+    private String jenisPemeriksaan;
+    private double harga;
     
 }
