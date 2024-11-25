5d6081ed2a12cfeae47837adf7db23efcae23d3a
Gisa Kayla <gisakayla.sy@gmail.com>
2024-11-25 14:54:36 +0700

    PendaftaranMCU done

--- web/src/main/java/com/mcu/web/models/PendaftaranMCU.java
+++ web/src/main/java/com/mcu/web/models/PendaftaranMCU.java
@@ -1,5 +1,31 @@
 package com.mcu.web.models;
 
+import lombok.*;
+import java.util.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class PendaftaranMCU {
-    
+    @Id
+    private String idPendaftaran;
+    private Date tanggalDaftar;
+    private String status;
+    @DBRef
+    @JsonIgnore
+    private Pasien pasien;
+    private PaketMCU paket;
+
+    public PendaftaranMCU(String id, Date tanggal, String status, Pasien pasien, PaketMCU paket) {
+        this.idPendaftaran = id;
+        this.tanggalDaftar = tanggal;
+        this.status = status;
+        this.pasien = pasien;
+        this.paket = paket;
+    }
+
 }
--- web/src/main/java/com/mcu/web/repositories/PendaftaranMCURepository.java
+++ web/src/main/java/com/mcu/web/repositories/PendaftaranMCURepository.java
@@ -1,5 +1,13 @@
 package com.mcu.web.repositories;
 
-public class PendaftaranMCURepository {
-    
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import com.mcu.web.models.PendaftaranMCU;
+import org.springframework.stereotype.Repository;
+
+
+public interface PendaftaranMCURepository extends MongoRepository<PendaftaranMCU, String>{
+    List<PendaftaranMCU> findByTanggalDaftarBetween(Date startDate, Date endDate);
 }
